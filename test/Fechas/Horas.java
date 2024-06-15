cd0fcec33c855ac83600f325f12eba4febddd308
Camila Martín <camilamartin1403@gmail.com>
2024-06-15 11:06:31 -0300

    agregar ficheros

--- test/Fechas/Horas.java
+++ test/Fechas/Horas.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Fechas;
 
-/**
- *
- * @author 2004c
- */
 public class Horas {
-    
+
+    /**
+     * devuelve la diferencia entre dos horas
+     * solo horas en punto y media hora (08:00, 08:30, 09:00)
+     * @param horaInicio hora de incio
+     * @param horaFin hora de fin
+     * @return Diferencia entre dos horas
+     */
+    public static double numeroHoras(String horaInicio, String horaFin) {
+
+        double numHoraInicio;
+        double numHoraFin;
+
+        String[] horaMinutoInicio = horaInicio.split(":");
+
+        numHoraInicio = Double.parseDouble(horaMinutoInicio[0]);
+        if (Integer.parseInt(horaMinutoInicio[1]) == 30) {
+            numHoraInicio += 0.5;
+        }
+
+        String[] horaMinutoFin = horaFin.split(":");
+
+        numHoraFin = Double.parseDouble(horaMinutoFin[0]);
+        if (Integer.parseInt(horaMinutoFin[1]) == 30) {
+            numHoraFin += 0.5;
+        }
+
+        return numHoraFin - numHoraInicio;
+
+    }
 }
+
--- test/Fechas/Validacion.java
+++ test/Fechas/Validacion.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Fechas;
 
-/**
- *
- * @author 2004c
- */
 public class Validacion {
+
+    /**
+     * Valida si la fecha es valida
+     * @param anio Año
+     * @param mes Mes
+     * @param dia Día
+     * @return True = fecha valida
+     */
+     public static boolean validarFecha(int anio, int mes, int dia){
+        
+        boolean valido=false;
+        
+        if (anio>=0 && (mes>=1 && mes<=12)){
+            if(numeroDeDiasMes(mes) >= dia){
+                valido=true;
+            }
+        }
+        
+        return valido;
+        
+    } 
+
+     /**
+      * Valida si la hora es correcta
+      * @param hora Hora
+      * @param minuto Minuto
+      * @param segundo Segundo
+      * @return True = hora correcta
+      */
+    public static boolean validaHora(int hora, int minuto, int segundo){
+        
+        boolean valido=false;
+        
+        if ((hora >= 0 && hora <= 23) && (minuto >= 0 && minuto <= 59) && (segundo >= 0 && segundo <= 59)){
+            valido = true;
+        }
+
+        return valido;
+    }
     
 }
